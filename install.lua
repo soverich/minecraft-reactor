fe57c5a4fc7d333472b4d729a4b14994a7b063ba
soverich <57356104+soverich@users.noreply.github.com>
2023-07-14 15:03:31 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,8 +1,7 @@
 local libURL = "https://raw.githubusercontent.com/soverich/minecraft-reactor/main/f.lua"
 local startupURL = "https://raw.githubusercontent.com/soverich/minecraft-reactor/main/reactor.lua"
-local monitorURL = "https://raw.githubusercontent.com/soverich/minecraft-reactor/main/moni.lua"
-local lib, startup, moni
-local libFile, startupFile, moniFile
+local lib, startup
+local libFile, startupFile
  
 fs.makeDir("lib")
  
@@ -20,10 +19,3 @@ startupFile = startup.readAll()
 local file2 = fs.open("startup", "w")
 file2.write(startupFile)
 file2.close()
-
-moni = http.get(monitorURL)
-moniFile = startup.readAll()
-
-local file3 = fs.open("moni", "w")
-file3 = write(moniFile)
-file3.close()
